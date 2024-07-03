57e2e9a760e3c94120527b61fd8029f7853689dd
Karthisk2000 <sskarthick662000@gmail.com>
2024-07-03 23:44:43 +0530

    Created login and home page

---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Projects() {
+    console.log('\n -------------------- projects ------------------------')
+
+    return (
+        <div>
+            Projects
+        </div>
+    )
+}
